eae7140f59882e84dbc989f00636d0243e1f86b5
Ovindi Vimasha <ovindivimasha1015@gmail.com>
2026-01-10 17:59:52 +0530

    Create Hero

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Navbar from '../components/layout/Navbar.jsx'
+import React from "react";
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/landing/Hero";
+import Features from "../components/landing/Features";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <Features />
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
