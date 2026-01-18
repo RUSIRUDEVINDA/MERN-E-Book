91d69d636a37e0ec33e61741b3438d0f1ceb1ffc
Ovindi Vimasha <ovindivimasha1015@gmail.com>
2026-01-18 20:04:22 +0530

    Bug Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,59 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import ProtectedRoute from './components/auth/ProtectedRoute'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignUpPage'
-import DashboardPage from './pages/DashboardPage'
-import EditorPage from './pages/EditorPage'
-import ViewBookPage from './pages/ViewBookPage'
-import ProfilePage from './pages/ProfilePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import DashboardPage from "./pages/DashboardPage";
+import EditorPage from "./pages/EditorPage";
+import ViewBookPage from "./pages/ViewBookPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        {/* Public Routes */ }
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
 
-        {/* Protected Routes */ }
-        <Route 
-          path='/dashboard'
-          element={<ProtectedRoute><DashboardPage /></ProtectedRoute>}  
-        />
-        <Route 
-          path='/editor/:bookId'
-          element={<ProtectedRoute><EditorPage /></ProtectedRoute>}  
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
         />
         <Route
-        path='viw-book/:bookId'
-        element={<ProtectedRoute><ViewBookPage /></ProtectedRoute>}  
+          path="/editor/:bookId"
+          element={
+            <ProtectedRoute>
+              <EditorPage />
+            </ProtectedRoute>
+          }
         />
         <Route
-        path='/profile'
-        element={<ProtectedRoute><ProfilePage /></ProtectedRoute>}  
+          path="/view-book/:bookId"
+          element={
+            <ProtectedRoute>
+              <ViewBookPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
         />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
