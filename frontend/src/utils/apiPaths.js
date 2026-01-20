2d33eac360cea95d47cec68f5c1ef4fb55ba7d55
Rusiru Devinda <116636125+RUSIRUDEVINDA@users.noreply.github.com>
2026-01-21 00:27:36 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://mern-e-book.onrender.com/api";
-export const BASE_URL = "https://mern-e-book.onrender.com";
+const API_BASE_URL = "https://mern-e-book-backend.onrender.com/api";
+export const BASE_URL = "https://mern-e-book-backend.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
